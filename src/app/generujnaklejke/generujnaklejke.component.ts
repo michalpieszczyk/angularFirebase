4f32845906792a3e26b44e2f87e5ec70bba0fe6a
unknown <michalpieszczyk@gmail.com>
2021-03-14 19:28:28 +0100

    aktualizacja naklejki

--- src/app/generujnaklejke/generujnaklejke.component.ts
+++ src/app/generujnaklejke/generujnaklejke.component.ts
@@ -102,7 +102,7 @@ public captureScreen()
       let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF  
       let position = 0;  
       pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
-      pdf.save('MYPdf.pdf'); // Generated PDF   
+      pdf.save('naklejka.pdf'); // Generated PDF   
     });  
   }  
 
